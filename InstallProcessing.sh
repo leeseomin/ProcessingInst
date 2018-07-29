48144d709cf3332076d0cc03164654aad168d404
AtjonTV <admin.atjontv@atvg-studios.at>
2018-07-29 03:16:33 +0200

    Bugfix and more
    * Fixing the bug where the Icon was set to `/opt/Processing/$version/` instead of the actual version.
    * Removing unneeded comments
    * Add a useful comment
    * Added more Categories

--- InstallProcessing.sh
+++ InstallProcessing.sh
@@ -4,34 +4,20 @@
 version=3.4
 
 echo This script will install Processing-$version-linux64 on Ubuntu 16.04.
-
-# Download Processing
 wget http://download.processing.org/processing-$version-linux64.tgz
-
-# Create folder 
 mkdir /opt/Processing
-
-# Unzip the tarball
 tar -xvzf processing-$version-linux64.tgz -C /opt/Processing
-
-# Rename the folder 
 mv /opt/Processing/processing-$version /opt/Processing/$version
-
-# Create a desktop icon for Processing 
 ln -s /opt/Processing/$version/processing /usr/local/bin/processing
-
-# Create a launcher file and input contents 
-echo '[Desktop Entry]
+echo "[Desktop Entry]
 Version=$version
 Name=Processing
-Comment=Processing :) 
-Exec=processing %F
+Comment=A flexible software sketchbook
+Exec=processing
 Icon=/opt/Processing/$version/lib/icons/pde-256.png
 Terminal=false
 Type=Application
-Categories=AudioVideo;Video;Graphics;' >  /usr/share/applications/processing.desktop
-
-# Create a mime type for .pde files. (might be an issue with arduino too?) 
+Categories=AudioVideo;Video;Graphics;Development;Animation;IDE" >  /usr/share/applications/processing.desktop
 echo '<?xml version="1.0" encoding="UTF-8"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
 <mime-type type="text/x-processing">
@@ -40,9 +26,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 <glob pattern="*.pde"/>
 </mime-type>
 </mime-info>' > /usr/share/mime/packages/processing.xml
-
 update-mime-database /usr/share/mime
-
 echo 'text/x-processing=processing.desktop' >> /usr/share/applications/defaults.list
 echo 'install complete'
 echo 'Script by crazy-logic, edit by AtjonTV'
